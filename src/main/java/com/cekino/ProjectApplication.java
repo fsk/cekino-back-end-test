111e21450a9f16e9d86ae15a90967b890e1c6bc1
frknshnklksz <furkankulaksiz68@gmail.com>
2020-11-20 03:31:50 +0300

    Pom.XML war packaging'e cevrildi. Main class'ta gerekli TomCat kodları yazıldı.

--- src/main/java/com/cekino/ProjectApplication.java
+++ src/main/java/com/cekino/ProjectApplication.java
@@ -2,16 +2,23 @@ package com.cekino;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableJpaRepositories
 @EnableSwagger2
-public class ProjectApplication {
+public class ProjectApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ProjectApplication.class, args);
+
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ProjectApplication.class);
+    }
 }
